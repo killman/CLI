cb958559a6c7c580013dccb4b8d971ef75e4c3a0
Killman <killman@dkcorp.ec>
2013-10-10 01:34:57 -0500

    update

--- cli.py
+++ cli.py
@@ -7,25 +7,6 @@ host = 'Router'
 initialization=main()
 initialization.init()
 
-"""while True:
-    user_input = raw_input(host +  " #> ")
-    splitit = user_input
-    
-    if splitit == "show user":
-        user = os.getlogin()
-        print (user)
-    
-    if splitit == "clear":
-        os.system('clear && cat motd') 
-
-    if splitit == 'show hostname':
-         os.system('hostname')
-
-    if splitit == 'show os':
-         os.system('uname -sr')
-    if splitit == 'exit':
-         exit()
-"""
 if __name__ == '__main__':
     shell=main()
     shell.init()
